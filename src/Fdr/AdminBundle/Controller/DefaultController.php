59152c42ddfc6f6702a36e380f1a782794649899
ismailkomay <ismailkomay@gmail.com>
2015-03-04 08:56:42 +0000

    ajout Bundle

---
+++ src/Fdr/AdminBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fdr\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('FdrAdminBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Fdr/AdminBundle/FdrAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fdr\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FdrAdminBundle extends Bundle
+{
+}
