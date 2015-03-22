5ee97448148fd21eb9bf1f73edfeaca0796736bb
ismailkomay <ismailkomay@gmail.com>
2015-03-22 18:37:27 +0000

    commit

--- src/Fdr/AdminBundle/Entity/MissionAffretement.php
+++ src/Fdr/AdminBundle/Entity/MissionAffretement.php
@@ -81,7 +81,6 @@ class MissionAffretement
      *
      * @ORM\Column(name="valdeclassurance", type="float", precision=2, scale=0, nullable=true)
      * @Assert\Type(type="numeric", message="La valeur du champs n'est pas valide.")
-     * @Assert\Currency
      */
     private $valdeclassurance;
 
